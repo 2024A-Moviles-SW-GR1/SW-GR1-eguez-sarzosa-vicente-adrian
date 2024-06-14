5cf10071af02127b789dc19f9a567549b1cb0f01
Adrian Eguez <eadepto@hotmail.com>
2024-06-14 15:20:20 -0500

    hasta menu y strings

--- 01-Clases/app/src/main/java/com/example/a2024aswgr1vaes/BListView.kt
+++ 01-Clases/app/src/main/java/com/example/a2024aswgr1vaes/BListView.kt
@@ -2,10 +2,46 @@ package com.example.a2024aswgr1vaes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.ListView
+import com.google.android.material.snackbar.Snackbar
 
 class BListView : AppCompatActivity() {
+    val arreglo = BBaseDatosMemoria.arregloBEntrenador
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_blist_view)
+        val listView = findViewById<ListView>(R.id.lv_list_view)
+        val adaptador = ArrayAdapter(
+            this, // contexto
+            android.R.layout.simple_list_item_1, // layout xml a usar
+            arreglo
+        )
+        listView.adapter = adaptador
+        adaptador.notifyDataSetChanged() // ACTUALIZA UI
+        val botonAnadirListView = findViewById<Button>(
+            R.id.btn_anadir_list_view
+        )
+        botonAnadirListView.setOnClickListener {
+            anadirEntrenador(adaptador)
+        }
+        registerForContextMenu(listView) // NUEVA LINEA
+    }
+    fun anadirEntrenador(
+        adaptador: ArrayAdapter<BEntrenador>
+    ){
+        arreglo.add(
+            BEntrenador(4,"Wendy","d@d.com")
+        )
+        adaptador.notifyDataSetChanged()
+    }
+    fun mostrarSnackbar(texto:String){
+        val snack = Snackbar.make(
+            findViewById(R.id.cl_ciclo_vida),
+            texto,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack.show()
     }
 }
